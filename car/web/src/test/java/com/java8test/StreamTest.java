ed29e379914957df51dbf34a34e61fecf1c96e12
lizhong <mylizhong@126.com>
2017-09-14 10:12:19 +0800

    sream test demo

--- car/web/src/test/java/com/java8test/StreamTest.java
+++ car/web/src/test/java/com/java8test/StreamTest.java
@@ -3,27 +3,61 @@ package com.java8test;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamTest {
 
+
     @Test
-    public void test1(){
-        int[] ints = {1,6,8,4,23,96,0};
-        IntStream.range(0,ints.length).forEach(System.out::println);
+    public void testForeach(){
+        Integer[] integers = {1,6,8,4,23,96,0};
+        List<Integer> list = Arrays.asList(integers);
+        list.stream().forEach((x)->{
+            System.out.println(x);
+        });
     }
     @Test
-    public void test2(){
+    public void testMap(){
         Integer[] ints = {1,6,8,4,23,96,0};
-//        IntSummaryStatistics statistics = IntStream.of(ints).summaryStatistics();
-//        int max = statistics.getMax();
-//        System.out.println(max);
+     Arrays.asList(ints).stream().map(x -> 2 * x).collect(Collectors.toList()).forEach(System.out::println);
+    }
+    @Test
+    public void testFilter(){
+        String[] strings = {"aaa","bbba","ccccb"};
+        List<String> stringList = Arrays.asList(strings);
+        stringList.stream().filter(str->str.endsWith("a")).forEach(System.out::println);
+    }
 
-        List<Integer> integers = Arrays.asList(ints);
-        IntSummaryStatistics intSummaryStatistics = integers.stream().mapToInt(x -> x).summaryStatistics();
+    @Test
+    public void testLimit(){
+        String[] strings = {"aaa","bbba","ccccb"};
+        List<String> stringList = Arrays.asList(strings);
+        stringList.stream().limit(1).forEach((x)-> System.out.println(x));
+    }
+
+    @Test
+    public void testSorted(){
+        String[] strings = {"aassssa","ba","ccccb"};
+        List<String> stringList = Arrays.asList(strings);
+        stringList.stream().sorted(Comparator.comparingInt(String::length)).forEach(System.out::println);
+    }
+    @Test
+    public void testTj(){
+        int[] integers = {1,6,8,4,23,96,0};
+        IntSummaryStatistics stats = IntStream.of(integers).summaryStatistics();
+        System.out.println("列表中最大的数 : " + stats.getMax());
+        System.out.println("列表中最小的数 : " + stats.getMin());
+        System.out.println("所有数之和 : " + stats.getSum());
+        System.out.println("平均数 : " + stats.getAverage());
+    }
+    @Test
+    public void testStream2XXXStream(){
 
     }
+
 }
